9e97f92b1650f399fdaf8db510ce97fafa7e9af3
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-04 17:11:38 +0100

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -3,7 +3,6 @@ $Env:PATH = '{{ inputs.path }}'
 echo $ENV:INPUT_URL
 echo $ENV:INPUT_PATH
 Invoke-WebRequest -Uri '$ENV:INPUT_URL' -OutFile '/$ENV:INPUT_PATH'
-whereis git
 git config --global user.name '$ENV:GITHUB_REPOSITORY_OWNER'
 git config --global user.email '$ENV:GITHUB_REPOSITORY_OWNER@users.noreply.github.com'
 git add -A
