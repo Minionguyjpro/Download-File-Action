1aa1d8d63c10ebec3c5e465745c6b440a2ece9eb
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-04 09:03:22 +0100

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -3,3 +3,8 @@ $Env:PATH = '{{ inputs.path }}'
 echo $ENV:INPUT_URL
 echo $ENV:INPUT_PATH
 Invoke-WebRequest -Uri '$ENV:INPUT_URL' -OutFile '/$ENV:INPUT_PATH'
+git config --global user.name '$ENV:GITHUB_REPOSITORY_OWNER'
+git config --global user.email '$ENV:GITHUB_REPOSITORY_OWNER@users.noreply.github.com'
+git add -A
+git commit -am "Downloaded the file from '$ENV:INPUT_URL'"
+git push      
